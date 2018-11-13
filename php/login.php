aa3e0a47febc2c6bd15a899d24da82df85de21f1
Prajwal Prashanth <prajapple@gmail.com>
2018-11-13 18:30:22 +0530

    added addinbox

--- php/login.php
+++ php/login.php
@@ -2,22 +2,20 @@
 include 'connect.php';
 $db = dbconnect();
 
-$name =$_POST['username'];
-$password =$_POST['password'];
+$name = $_POST['username'];
+$password = $_POST['password'];
 
 $password_query = "SELECT password FROM users WHERE username='$name'";
-
 $stored_password = mysqli_query($db,$password_query);
 
 if($password == $stored_password)
 {
-
 	include ''; // next webpage
 }
 else
 {
 	include 'index.php'; // current webpage
-	echo '<script> alert("You have Entered Wrong Username/Password, Please Try Again!")</script>'; // alert or a pop up to notify
+	echo '<script> alert("You Have Entered Wrong Username/Password, Please Try Again!")</script>'; // alert or a pop up to notify
 }
 
 ?>
